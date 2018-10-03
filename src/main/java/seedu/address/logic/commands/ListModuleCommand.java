d512cf7370da40a50ff46ecfdc6a26db44518665
Hilda-Ang <E0196709@u.nus.edu>
2018-10-03 16:36:10 +0800

    ListModuleCommand.java: Add constructor and attributes needed for parsing

--- src/main/java/seedu/address/logic/commands/ListModuleCommand.java
+++ src/main/java/seedu/address/logic/commands/ListModuleCommand.java
@@ -3,6 +3,9 @@ package seedu.address.logic.commands;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SEMESTER;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_YEAR;
+
 /**
  * Placeholder for ListModuleCommand.
  * Lists all modules the user has taken or is available to take.
@@ -10,6 +13,24 @@ import seedu.address.model.Model;
  * @author Hilda-Ang
  */
 public class ListModuleCommand extends Command {
+
+    public static final String COMMAND_WORD = "list";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": List modules taken or suggested for a semester. "
+            + "Parameters: "
+            + PREFIX_YEAR + "YEAR "
+            + PREFIX_SEMESTER + "SEMESTER "
+            + "Example: " + COMMAND_WORD + " "
+            + PREFIX_YEAR + "1 "
+            + PREFIX_SEMESTER + "1 ";
+
+    /**
+     * Creates a ListModuleCommand to list modules for specified semester.
+     */
+    public ListModuleCommand(int year, int semester) {
+        //TODO
+    }
+
     //TODO
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
