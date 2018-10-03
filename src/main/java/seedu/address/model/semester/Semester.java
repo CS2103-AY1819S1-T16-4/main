2386aab7077a17fe88745f7b6657a8469a475f54
Hilda-Ang <E0196709@u.nus.edu>
2018-10-03 10:15:59 +0800

    Create Semester class

---
+++ src/main/java/seedu/address/model/semester/Semester.java
@@ -0,0 +1,11 @@
+package seedu.address.model.semester;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+/**
+ * Represents a Semester in the module planner.
+ * Guarantees: details are present and not null.
+ */
+public class Semester {
+
+}
