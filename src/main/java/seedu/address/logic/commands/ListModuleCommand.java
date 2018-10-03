facf73bd9a3a4ac2df4eb59c1542d4219d85ba24
Hilda-Ang <E0196709@u.nus.edu>
2018-10-03 16:14:56 +0800

    Add a placeholder for ListModuleCommand class

---
+++ src/main/java/seedu/address/logic/commands/ListModuleCommand.java
@@ -0,0 +1,18 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+/**
+ * Placeholder for ListModuleCommand.
+ * Lists all modules the user has taken or is available to take.
+ *
+ * @author Hilda-Ang
+ */
+public class ListModuleCommand extends Command {
+    //TODO
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        return null;
+    }
+}
