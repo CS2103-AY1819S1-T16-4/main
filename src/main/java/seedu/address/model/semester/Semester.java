1cbb948b5036c50252f662d24b6b2148f3fed30c
Hilda-Ang <E0196709@u.nus.edu>
2018-10-03 10:27:27 +0800

    Semester.java: Add attributes and constructor

--- src/main/java/seedu/address/model/semester/Semester.java
+++ src/main/java/seedu/address/model/semester/Semester.java
@@ -1,11 +1,36 @@
 package seedu.address.model.semester;
 
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import java.util.ArrayList;
+import java.util.List;
+
+import seedu.address.model.module.Module;
 
 /**
  * Represents a Semester in the module planner.
- * Guarantees: details are present and not null.
  */
 public class Semester {
 
+	// Basic information
+	private final int semester;
+	private final int year;
+
+	// Indicator of whether user has passed the semester
+	private boolean past;
+
+	// Modules lists
+	private List<Module> modulesTaken = new ArrayList<Module>();
+	private List<Module> modulesSuggested = new ArrayList<Module>();
+
+	/**
+     * Constructs a {@code Semester}.
+     *
+     * @param semester A valid semester.
+     * @param year A valid year.
+     * @param past An indicator of whether the semester has been passed.
+     */
+    public Semester(int semester, int year, boolean past) {
+        this.semester = semester;
+        this.year = year;
+        this.past = past;
+    } 
 }
