3f689bf7ef8115d44b07c5b0a6a85d203fa2843d
Gabriel Yik Ren Jun <gabrielyik@yahoo.com.sg>
2018-10-06 22:20:33 +0800

    Partially implement the Module and ModuleInformation classes

--- src/main/java/seedu/planner/model/module/Module.java
+++ src/main/java/seedu/planner/model/module/Module.java
@@ -5,4 +5,43 @@ package seedu.planner.model.module;
 /**
  * Temporary {@code Module} class placeholder.
  */
-public class Module {}
+public class Module {
+
+    private ModuleType type;
+
+    private ModuleInformation information;
+
+    public Module(ModuleType type, ModuleInformation information) {
+        this.type = type;
+        this.information = information;
+    }
+
+    public String getCode() {
+        return information.getCode();
+    }
+
+    public String getName() {
+        return information.getName();
+    }
+
+    public ModuleType getType() {
+        return type;
+    }
+
+    public ModuleType[] getPossibleTypes() {
+        return information.getPossibleTypes();
+    }
+
+    public int getCreditCount() {
+        return information.getCreditCount();
+    }
+
+    public Module[] getPreclusions() {
+        return information.getPreclusions();
+    }
+
+    public Module[] getPrerequisites() {
+        return information.getPrerequisites();
+    }
+
+}
--- src/main/java/seedu/planner/model/module/ModuleInformation.java
+++ src/main/java/seedu/planner/model/module/ModuleInformation.java
@@ -10,4 +10,50 @@ public class ModuleInformation {
             + "where WXY refers to a prefix like CS or IS that describes the type of the module, "
             + "1234 refers to a sequence of positive numbers, "
             + "and (Z) refers to an optional postfix.";
+
+    private String code;
+
+    private String name;
+
+    private ModuleType[] possibleTypes;
+
+    private int creditCount;
+
+    private Module[] preclusions;
+
+    private Module[] prerequisites;
+
+    private ModuleInformation(String code, String name, ModuleType[] possibleTypes,
+                              int creditCount, Module[] preclusions, Module[] prerequisites) {
+        this.code = code;
+        this.name = name;
+        this.possibleTypes = possibleTypes;
+        this.creditCount = creditCount;
+        this.preclusions = preclusions;
+        this.prerequisites = prerequisites;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ModuleType[] getPossibleTypes() {
+        return possibleTypes;
+    }
+
+    public int getCreditCount() {
+        return creditCount;
+    }
+
+    public Module[] getPreclusions() {
+        return preclusions;
+    }
+
+    public Module[] getPrerequisites() {
+        return prerequisites;
+    }
 }
