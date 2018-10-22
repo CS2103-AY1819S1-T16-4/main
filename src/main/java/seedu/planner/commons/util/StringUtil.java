c4859ec2d8e3afd1bf7364a843cb2915b7a55dce
Gabriel Yik Ren Jun <gabrielyik@yahoo.com.sg>
2018-10-22 20:37:27 +0800

    StringUtil.java: Update regex to be stricter

--- src/main/java/seedu/planner/commons/util/StringUtil.java
+++ src/main/java/seedu/planner/commons/util/StringUtil.java
@@ -13,7 +13,7 @@ import java.util.Collection;
  */
 public class StringUtil {
 
-    private static final String ALL_LETTERS_REGEX = "[a-zA-Z ]+";
+    private static final String ALL_LETTERS_REGEX = "[a-zA-Z]+( +[a-zA-Z]+)*";
 
     /**
      * Returns true if the {@code sentence} contains the {@code word}.
@@ -81,18 +81,23 @@ public class StringUtil {
      * @return True if the String contains only alphabets
      */
     public static boolean containsOnlyLettersAndWhiteSpace(String s) {
+        requireNonNull(s);
         return s.matches(ALL_LETTERS_REGEX);
     }
 
     /**
      * Takes the elements in a collection and combines them into a string
      * with each element separated by a whitespace.
+     * The class of the element has to override the {@code toString()} method
+     * for this method to work properly.
      *
      * @param collection The collection
      * @param <E> The runtime type of the collection
      * @return The string containing all the elements in the collection
      */
     public static <E> String convertCollectionToString(Collection<E> collection) {
+        requireNonNull(collection);
+
         StringBuilder sb = new StringBuilder();
         for (E e : collection) {
             sb.append(e + " ");
