bc7141c3eb5ce52d1b7fbc28fa24172566b8c6c4
Hilda-Ang <E0196709@u.nus.edu>
2018-10-24 18:12:56 +0800

    Remove unused method

--- src/main/java/seedu/planner/logic/Logic.java
+++ src/main/java/seedu/planner/logic/Logic.java
@@ -20,9 +20,6 @@ public interface Logic {
      */
     CommandResult execute(String commandText) throws CommandException, ParseException;
 
-    /** Returns an unmodifiable view of the filtered list of persons */
-    ObservableList<Person> getFilteredPersonList();
-
     //@@author GabrielYik
 
     //TODO: confirm if the list is filtered or sorted or both
