f6d2cb45f900fae2de2a4ae5ce2fef02439c844b
Gabriel Yik Ren Jun <gabrielyik@yahoo.com.sg>
2018-10-26 00:34:02 +0800

    Move classes associated with the user into the package "user"

---
+++ src/main/java/seedu/planner/model/user/exceptions/UserProfileNotSetUpException.java
@@ -0,0 +1,8 @@
+package seedu.planner.model.user.exceptions;
+
+/**
+ * Signals that the user profile is not yet set up.
+ */
+public class UserProfileNotSetUpException extends RuntimeException {
+
+}
