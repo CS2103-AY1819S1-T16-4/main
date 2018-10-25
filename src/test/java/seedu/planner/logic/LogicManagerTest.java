7f68310725bb718b114038a0d8c09eed5c4cbedf
Hilda-Ang <E0196709@u.nus.edu>
2018-10-25 14:22:33 +0800

    Fix failing test

--- src/test/java/seedu/planner/logic/LogicManagerTest.java
+++ src/test/java/seedu/planner/logic/LogicManagerTest.java
@@ -8,9 +8,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.planner.logic.commands.ClearCommand;
 import seedu.planner.logic.commands.CommandResult;
 import seedu.planner.logic.commands.HistoryCommand;
-import seedu.planner.logic.commands.ListModuleCommand;
 import seedu.planner.logic.commands.exceptions.CommandException;
 import seedu.planner.logic.parser.exceptions.ParseException;
 import seedu.planner.model.Model;
@@ -41,9 +41,9 @@ public class LogicManagerTest {
 
     @Test
     public void execute_validCommand_success() {
-        String listModuleCommand = ListModuleCommand.COMMAND_WORD;
-        assertCommandSuccess(listModuleCommand, ListModuleCommand.MESSAGE_SUCCESS, model);
-        assertHistoryCorrect(listModuleCommand);
+        String clearCommand = ClearCommand.COMMAND_WORD;
+        assertCommandSuccess(clearCommand, ClearCommand.MESSAGE_SUCCESS, model);
+        assertHistoryCorrect(clearCommand);
     }
 
     /*
