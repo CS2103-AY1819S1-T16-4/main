19153ee740354cf230945220cb4276729623b5f6
Gabriel Yik Ren Jun <gabrielyik@yahoo.com.sg>
2018-10-26 22:23:04 +0800

    Add support for Find command in UI

--- src/main/java/seedu/planner/ui/FindModulePanel.java
+++ src/main/java/seedu/planner/ui/FindModulePanel.java
@@ -9,85 +9,16 @@ import seedu.planner.model.module.ModuleInfo;
 
 //@@author GabrielYik
 
-//TODO: a lot of similarities with ModuleListCard --> need to abstract out
-
 /**
  * A ui for the find module panel that is displayed
  * on the right of the application.
  * This panel corresponds to the {@code find} command.
  */
-
-/*
-public class FindModulePanel extends UiPart<Region> {
+public class FindModulePanel extends ModuleDescription {
     private static final String FXML = "FindModulePanel.fxml";
 
-    public final Module module;
-
-    @javafx.fxml.FXML
-    private Label moduleCode;
-
-    @FXML
-    private Label moduleName;
-
-    @FXML
-    private Label moduleType;
-
-    @FXML
-    private Label creditCount;
-
-    @FXML
-    private Label preclusion;
-
-    @FXML
-    private FlowPane preclusions;
-
-    @FXML
-    private Label prerequisite;
-
-    @FXML
-    private FlowPane prerequisites;
-
     public FindModulePanel(Module module) {
-        super(FXML);
-        this.module = module;
-        moduleCode.setText(module.getCode());
-        moduleName.setText(module.getName());
-        moduleType.setText("Fulfils: " + module.getType().toString());
-        creditCount.setText("Modular Credits: " + Integer.toString(module.getCreditCount()));
-
-        for (ModuleInfo m : module.getPreclusions()) {
-            preclusions.getChildren().add(new Label(m.getCode()));
-        }
-
-        switch(module.getPreclusions().length) {
-        case 0:
-            preclusion.setText("Preclusion: none");
-            break;
-
-        case 1:
-            preclusion.setText("Preclusion: ");
-            break;
-
-        default:
-            preclusion.setText("Preclusions: ");
-        }
-
-        for (ModuleInfo m : module.getPrerequisites()) {
-            prerequisites.getChildren().add(new Label(m.getCode()));
-        }
-
-        switch(module.getPrerequisites().length) {
-        case 0:
-            prerequisite.setText("Prerequisite: none");
-            break;
-
-        case 1:
-            prerequisite.setText("Prerequisite: ");
-            break;
-
-        default:
-            prerequisite.setText("Prerequisites: ");
-        }
+        super(module, FXML);
     }
 
     @Override
@@ -98,13 +29,12 @@ public class FindModulePanel extends UiPart<Region> {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof ModuleListCard)) {
+        if (!(other instanceof FindModulePanel)) {
             return false;
         }
 
         // state check
         FindModulePanel panel = (FindModulePanel) other;
-        return moduleCode.equals(panel.moduleCode);
+        return module.equals(panel.module);
     }
 }
-*/
