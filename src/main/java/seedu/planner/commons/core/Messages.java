840b6460dc9d93421a008c9357fd4c375a595274
Gabriel Yik Ren Jun <gabrielyik@yahoo.com.sg>
2018-10-28 00:38:17 +0800

    Messages.java: Update format specifier for consistency

--- src/main/java/seedu/planner/commons/core/Messages.java
+++ src/main/java/seedu/planner/commons/core/Messages.java
@@ -10,6 +10,6 @@ public class Messages {
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
 
-    public static final String MESSAGE_INVALID_MODULES = "Invalid modules: %s";
+    public static final String MESSAGE_INVALID_MODULES = "Invalid modules: %1$s";
     public static final String MESSAGE_INVALID_PARAMETERS = "Invalid parameters";
 }
