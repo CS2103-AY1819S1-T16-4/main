87688e620c6aa2637529ed2d2878d38f5fc9de48
RomaRomama <rezkyarizaputra@yahoo.co.id>
2018-10-28 23:04:37 +0800

    Add temporary status command

---
+++ src/main/java/seedu/planner/logic/commands/StatusCommand.java
@@ -0,0 +1,46 @@
+package seedu.planner.logic.commands;
+
+import seedu.planner.logic.CommandHistory;
+import seedu.planner.model.Model;
+import seedu.planner.model.module.Module;
+
+import static java.util.Objects.requireNonNull;
+
+public class StatusCommand extends Command {
+
+    public static final String COMMAND_WORD = "status";
+    private static final String CREDITS_LEFT = "Total credits left to fulfill course requirement: ";
+
+    private static final int CREDIT_TO_GRADUATE = 160;
+    private static final int NUM_OF_SEMESTER = 8;
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows total credit achieved "
+            + "in each semester based on"
+            + "existing modules in the planner.\n"
+            + "Example: " + "Semester 1: 20\n"
+            + "Semester 2: 24\n"
+            + CREDITS_LEFT + 116;
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        requireNonNull(model);
+
+        int[] creditCounts = new int[NUM_OF_SEMESTER];
+        int totalCreditCount = 0;
+        for (int i = 0; i < NUM_OF_SEMESTER; i++) {
+            for (Module module : model.getTakenModuleList(i)) {
+                creditCounts[i] += module.getCreditCount();
+            }
+            totalCreditCount += creditCounts[i];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < NUM_OF_SEMESTER; i++) {
+            sb.append("Semester " + i + ": " + creditCounts[i - 1] + "\n");
+        }
+        sb.append(CREDITS_LEFT + (CREDIT_TO_GRADUATE - totalCreditCount));
+
+        return new CommandResult(sb.toString().trim());
+    }
+
+}
