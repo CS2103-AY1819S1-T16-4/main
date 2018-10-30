464955188b8dba577aa27509d410363b96b52e3b
Hilda-Ang <E0196709@u.nus.edu>
2018-10-30 13:11:52 +0800

    Add tests for ModulePlannerParser

--- src/test/java/seedu/planner/logic/parser/ModulePlannerParserTest.java
+++ src/test/java/seedu/planner/logic/parser/ModulePlannerParserTest.java
@@ -9,11 +9,19 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import seedu.planner.logic.commands.AddCommand;
 import seedu.planner.logic.commands.ClearCommand;
+import seedu.planner.logic.commands.DeleteCommand;
 import seedu.planner.logic.commands.ExitCommand;
+import seedu.planner.logic.commands.FindCommand;
+import seedu.planner.logic.commands.GoToCommand;
 import seedu.planner.logic.commands.HelpCommand;
 import seedu.planner.logic.commands.HistoryCommand;
+import seedu.planner.logic.commands.ListCommand;
 import seedu.planner.logic.commands.RedoCommand;
+import seedu.planner.logic.commands.SetUpCommand;
+import seedu.planner.logic.commands.StatusCommand;
+import seedu.planner.logic.commands.SuggestCommand;
 import seedu.planner.logic.commands.UndoCommand;
 import seedu.planner.logic.parser.exceptions.ParseException;
 
@@ -24,15 +32,39 @@ public class ModulePlannerParserTest {
     private final ModulePlannerParser parser = new ModulePlannerParser();
 
     @Test
+    public void parseCommand_add() throws Exception {
+        assertTrue(parser.parseCommand(AddCommand.COMMAND_WORD) instanceof AddCommand);
+        assertTrue(parser.parseCommand(AddCommand.COMMAND_WORD + " 3") instanceof AddCommand);
+    }
+
+    @Test
     public void parseCommand_clear() throws Exception {
         assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD) instanceof ClearCommand);
         assertTrue(parser.parseCommand(ClearCommand.COMMAND_WORD + " 3") instanceof ClearCommand);
     }
 
     @Test
+    public void parseCommand_delete() throws Exception {
+        assertTrue(parser.parseCommand(DeleteCommand.COMMAND_WORD) instanceof DeleteCommand);
+        assertTrue(parser.parseCommand(DeleteCommand.COMMAND_WORD + " 3") instanceof DeleteCommand);
+    }
+
+    @Test
     public void parseCommand_exit() throws Exception {
         assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD) instanceof ExitCommand);
         assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD + " 3") instanceof ExitCommand);
+    }
+
+    @Test
+    public void parseCommand_find() throws Exception {
+        assertTrue(parser.parseCommand(FindCommand.COMMAND_WORD) instanceof FindCommand);
+        assertTrue(parser.parseCommand(FindCommand.COMMAND_WORD + " 3") instanceof FindCommand);
+    }
+
+    @Test
+    public void parseCommand_goTo() throws Exception {
+        assertTrue(parser.parseCommand(GoToCommand.COMMAND_WORD) instanceof GoToCommand);
+        assertTrue(parser.parseCommand(GoToCommand.COMMAND_WORD + " 3") instanceof GoToCommand);
     }
 
     @Test
@@ -55,9 +87,33 @@ public class ModulePlannerParserTest {
     }
 
     @Test
+    public void parseCommand_list() throws Exception {
+        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD) instanceof ListCommand);
+        assertTrue(parser.parseCommand(ListCommand.COMMAND_WORD + " 3") instanceof ListCommand);
+    }
+
+    @Test
     public void parseCommand_redoCommandWord_returnsRedoCommand() throws Exception {
         assertTrue(parser.parseCommand(RedoCommand.COMMAND_WORD) instanceof RedoCommand);
         assertTrue(parser.parseCommand("redo 1") instanceof RedoCommand);
+    }
+
+    @Test
+    public void parseCommand_setUp() throws Exception {
+        assertTrue(parser.parseCommand(SetUpCommand.COMMAND_WORD) instanceof SetUpCommand);
+        assertTrue(parser.parseCommand(SetUpCommand.COMMAND_WORD + " 3") instanceof SetUpCommand);
+    }
+
+    @Test
+    public void parseCommand_status() throws Exception {
+        assertTrue(parser.parseCommand(StatusCommand.COMMAND_WORD) instanceof StatusCommand);
+        assertTrue(parser.parseCommand(StatusCommand.COMMAND_WORD + " 3") instanceof StatusCommand);
+    }
+
+    @Test
+    public void parseCommand_suggest() throws Exception {
+        assertTrue(parser.parseCommand(SuggestCommand.COMMAND_WORD) instanceof SuggestCommand);
+        assertTrue(parser.parseCommand(SuggestCommand.COMMAND_WORD + " 3") instanceof SuggestCommand);
     }
 
     @Test
