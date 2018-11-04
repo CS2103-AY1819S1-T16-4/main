2d098070a38012795f9abb94a5f91ba699e33fbe
Loo Rong Jie <loorongjie@gmail.com>
2018-11-04 20:36:32 +0800

    fix style

--- src/test/java/seedu/planner/model/module/ModuleInfoTest.java
+++ src/test/java/seedu/planner/model/module/ModuleInfoTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class ModuleInfoTest {
     @Test
-    public void ToString() {
+    public void printToString() {
         // Sanity check to make sure toString does not crash.
         System.out.println(ModuleInfo.getFromModuleCode("CS1010").get());
     }
