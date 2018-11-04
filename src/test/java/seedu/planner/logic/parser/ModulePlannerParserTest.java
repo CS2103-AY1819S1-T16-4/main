bd9ec93e429fac38ceaac2c6815f9fc6efeb0344
RomaRomama <rezkyarizaputra@yahoo.co.id>
2018-11-04 21:51:12 +0800

    Fix unimported variables

--- src/test/java/seedu/planner/logic/parser/ModulePlannerParserTest.java
+++ src/test/java/seedu/planner/logic/parser/ModulePlannerParserTest.java
@@ -11,6 +11,8 @@ import static seedu.planner.logic.commands.CommandTestUtil.VALID_MAJOR_CS;
 import static seedu.planner.logic.commands.CommandTestUtil.VALID_MAJOR_DESC_CS;
 import static seedu.planner.logic.commands.CommandTestUtil.VALID_MODULE_CODE_DESC_CS1010;
 import static seedu.planner.logic.commands.CommandTestUtil.VALID_MODULE_CODE_DESC_CS2030;
+import static seedu.planner.logic.commands.CommandTestUtil.VALID_MODULE_CS1010;
+import static seedu.planner.logic.commands.CommandTestUtil.VALID_MODULE_CS2030;
 import static seedu.planner.logic.commands.CommandTestUtil.VALID_SEMESTER_DESC_ONE;
 import static seedu.planner.logic.commands.CommandTestUtil.VALID_SEMESTER_ONE;
 import static seedu.planner.logic.commands.CommandTestUtil.VALID_YEAR_DESC_ONE;
