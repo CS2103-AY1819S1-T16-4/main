fcbc9af5f068b37c9dde3db9ce1999eb4f3af8f5
GabrielYik <gabrielyik@yahoo.com.sg>
2018-11-05 22:16:36 +0800

    Add in description of module in ui

--- src/main/java/seedu/planner/ui/FindModulePanel.java
+++ src/main/java/seedu/planner/ui/FindModulePanel.java
@@ -1,5 +1,8 @@
 package seedu.planner.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 import seedu.planner.model.module.Module;
 
 //@@author GabrielYik
@@ -12,8 +15,21 @@ import seedu.planner.model.module.Module;
 public class FindModulePanel extends ModuleDescription {
     private static final String FXML = "FindModulePanel.fxml";
 
+    @FXML
+    private Label divider;
+
+    @FXML
+    private Label fullDescription;
+
+    @FXML
+    private Label description;
+
     public FindModulePanel(Module module) {
         super(module, FXML);
+
+        divider.setText(System.lineSeparator());
+        fullDescription.setText("Full Description");
+        description.setText(module.getDescription());
     }
 
     @Override
