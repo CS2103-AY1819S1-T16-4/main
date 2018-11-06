4a7dba3696ab4cd8bee88b2036e53a2b38f60df5
Hilda-Ang <E0196709@u.nus.edu>
2018-11-06 12:07:02 +0800

    Add tests

--- src/test/java/seedu/planner/logic/parser/ListCommandParserTest.java
+++ src/test/java/seedu/planner/logic/parser/ListCommandParserTest.java
@@ -9,6 +9,7 @@ import static seedu.planner.logic.commands.CommandTestUtil.VALID_YEAR_DESC_ONE;
 import static seedu.planner.logic.commands.CommandTestUtil.VALID_YEAR_ONE;
 import static seedu.planner.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.planner.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.planner.model.util.IndexUtil.VALUE_NOT_AVAILABLE;
 
 import org.junit.Test;
 
@@ -21,8 +22,13 @@ public class ListCommandParserTest {
     private ListCommandParser parser = new ListCommandParser();
 
     @Test
+    public void parse_noFieldPresent_success() {
+        assertParseSuccess(parser, PREAMBLE_WHITESPACE,
+            new ListCommand(VALUE_NOT_AVAILABLE));
+    }
+
+    @Test
     public void parse_allFieldsPresent_success() {
-        // whitespace only preamble
         assertParseSuccess(parser, PREAMBLE_WHITESPACE + VALID_YEAR_DESC_ONE,
             new ListCommand(VALID_YEAR_ONE));
     }
@@ -34,7 +40,7 @@ public class ListCommandParserTest {
             ParserUtil.MESSAGE_INVALID_YEAR);
 
         // non-empty preamble
-        assertParseFailure(parser, PREAMBLE_NON_EMPTY + VALID_YEAR_DESC_ONE + VALID_SEMESTER_DESC_ONE,
+        assertParseFailure(parser, PREAMBLE_NON_EMPTY + VALID_YEAR_DESC_ONE,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, ListCommand.MESSAGE_USAGE));
     }
 }
