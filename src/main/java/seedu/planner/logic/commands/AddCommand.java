f24623a4ce082758a077b7a550f1a0419027b06b
RomaRomama <rezkyarizaputra@yahoo.co.id>
2018-11-06 18:54:53 +0800

    fIx CHecKstYle

--- src/main/java/seedu/planner/logic/commands/AddCommand.java
+++ src/main/java/seedu/planner/logic/commands/AddCommand.java
@@ -1,8 +1,8 @@
 package seedu.planner.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.planner.commons.util.CollectionUtil.convertCollectionToString;
 import static seedu.planner.commons.util.CollectionUtil.areEqualIgnoreOrder;
+import static seedu.planner.commons.util.CollectionUtil.convertCollectionToString;
 import static seedu.planner.commons.util.CollectionUtil.formatMessage;
 import static seedu.planner.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.planner.logic.parser.CliSyntax.PREFIX_CODE;
