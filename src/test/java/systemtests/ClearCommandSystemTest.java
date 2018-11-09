011b984c656944f23414c0831e616fc98c56e88e
Hilda-Ang <E0196709@u.nus.edu>
2018-11-09 16:54:50 +0800

    Revert tests in ClearCommandSystemTest

--- src/test/java/systemtests/ClearCommandSystemTest.java
+++ src/test/java/systemtests/ClearCommandSystemTest.java
@@ -1,39 +1,38 @@
 package systemtests;
 
+import org.junit.Test;
+
 import seedu.planner.logic.commands.ClearCommand;
+import seedu.planner.logic.commands.RedoCommand;
+import seedu.planner.logic.commands.UndoCommand;
 import seedu.planner.model.Model;
 import seedu.planner.model.ModelManager;
 
+import static seedu.planner.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+
 public class ClearCommandSystemTest extends ModulePlannerSystemTest {
 
-    /*
     @Test
     public void clear() {
         final Model defaultModel = getModel();
-    */
+        
     /* Case: clear non-empty planner book, command with leading spaces and trailing alphanumeric characters and
      * spaces -> cleared
      */
-    /*
-    assertCommandSuccess("   " + ClearCommand.COMMAND_WORD + " ab12   ");
+    assertCommandSuccess("   " + ClearCommand.COMMAND_WORD);
     assertSelectedCardUnchanged();
-    */
 
     /* Case: undo clearing planner book -> original planner book restored */
-    /*
     String command = UndoCommand.COMMAND_WORD;
     String expectedResultMessage = UndoCommand.MESSAGE_SUCCESS;
     assertCommandSuccess(command, expectedResultMessage, defaultModel);
     assertSelectedCardUnchanged();
-    */
 
     /* Case: redo clearing planner book -> cleared */
-    /*
     command = RedoCommand.COMMAND_WORD;
     expectedResultMessage = RedoCommand.MESSAGE_SUCCESS;
     assertCommandSuccess(command, expectedResultMessage, new ModelManager());
     assertSelectedCardUnchanged();
-    */
 
     /* Case: selects first card in person list and clears planner book -> cleared and no card selected */
     /*
@@ -42,7 +41,7 @@ public class ClearCommandSystemTest extends ModulePlannerSystemTest {
     assertCommandSuccess(ClearCommand.COMMAND_WORD);
     assertSelectedCardDeselected();
     */
-
+    
     /* Case: filters the person list before clearing -> entire planner book cleared */
     /*
     executeCommand(UndoCommand.COMMAND_WORD); // restores the original planner book
@@ -52,24 +51,18 @@ public class ClearCommandSystemTest extends ModulePlannerSystemTest {
     */
 
     /* Case: clear empty planner book -> cleared */
-    /*
     assertCommandSuccess(ClearCommand.COMMAND_WORD);
     assertSelectedCardUnchanged();
-    */
 
     /* Case: mixed case command word -> rejected */
-    /*
     assertCommandFailure("ClEaR", MESSAGE_UNKNOWN_COMMAND);
-    */
-    /*
     }
-    */
 
     /**
      * Executes {@code command} and verifies that the command box displays an empty string, the result display
      * box displays {@code ClearCommand#MESSAGE_SUCCESS} and the model related components equal to an empty model.
      * These verifications are done by
-     * {@code AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
+     * {@code ModulePlannerSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
      * Also verifies that the command box has the default style class and the status bar's sync status changes.
      * @see ModulePlannerSystemTest#assertApplicationDisplaysExpected(String, String, Model)
      */
@@ -93,7 +86,7 @@ public class ClearCommandSystemTest extends ModulePlannerSystemTest {
      * Executes {@code command} and verifies that the command box displays {@code command}, the result display
      * box displays {@code expectedResultMessage} and the model related components equal to the current model.
      * These verifications are done by
-     * {@code AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
+     * {@code ModulePlannerSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
      * Also verifies that the browser url, selected card and status bar remain unchanged, and the command box has the
      * error style.
      * @see ModulePlannerSystemTest#assertApplicationDisplaysExpected(String, String, Model)
