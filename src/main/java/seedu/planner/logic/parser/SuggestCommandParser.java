37d9a58890043fd561d46ce875f6c55312f1b8d5
Hilda-Ang <E0196709@u.nus.edu>
2018-11-11 18:37:35 +0800

    Fix typo in comments

--- src/main/java/seedu/planner/logic/parser/SuggestCommandParser.java
+++ src/main/java/seedu/planner/logic/parser/SuggestCommandParser.java
@@ -16,8 +16,8 @@ import seedu.planner.model.util.IndexUtil;
 public class SuggestCommandParser implements Parser<SuggestCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the ListCommand
-     * and returns a ListCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the SuggestCommand
+     * and returns a SuggestCommand object for execution.
      *
      * @throws ParseException if the user input does not conform the expected format
      */
