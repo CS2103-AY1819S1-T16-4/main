e294e13c92724fd5f6d2c054ac01190b7fb237c7
Hilda Anggraeni <31788611+Hilda-Ang@users.noreply.github.com>
2018-11-12 10:13:08 +0800

    Reposition comment in ListModulesEvent

--- src/main/java/seedu/planner/commons/events/ui/ListModulesEvent.java
+++ src/main/java/seedu/planner/commons/events/ui/ListModulesEvent.java
@@ -6,7 +6,8 @@ import seedu.planner.commons.events.BaseEvent;
  * An event to display list of modules taken in response to list command.
  */
 public class ListModulesEvent extends BaseEvent {
-    public static final int ALL_YEARS = -1; // indicates that user requested to see list of taken modules for all years
+    // indicates that user requested to see list of taken modules for all years
+    public static final int ALL_YEARS = -1;
 
     private final int year;
 
